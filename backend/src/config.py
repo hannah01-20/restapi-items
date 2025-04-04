54fc8cedd67aaaf87e4430ab894c8d434fb3b978
Hannah <hannahg.florano@gmail.com>
2025-04-04 14:05:50 +0800

    refactoring

--- backend/src/config.py
+++ backend/src/config.py
@@ -5,15 +5,15 @@ load_dotenv()
 
 # development setting
 DEBUG = os.getenv("DEBUG_MODE", False)
-SECRET_KEY = os.getenv("SECRET_KEY", "hashvrdascvfhtaajhgfvsdva56678nnnn345nhhsdffhhsdgweghhhh34t3rgver")
+SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
 
 # Database
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_HOST", "sqlite:///db.sqlite3")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
 
 # CORS
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
 # JWT
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "7f907b7520997c0b8a0f3163f620382ee45690043958a85e2f9a8391983ead1a")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_jwt_secret_key")
 JWT_TOKEN_LOCATION = ["headers"]
 JWT_IDENTITY_CLAIM = "user_id" # sub
