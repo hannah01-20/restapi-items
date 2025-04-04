9655436fd0e4a52142f81858c9890c357d9436fe
Hannah <hannahg.florano@gmail.com>
2025-04-04 12:39:42 +0800

    refactored item update, delete toast

--- frontend/src/services/itemApi.tsx
+++ frontend/src/services/itemApi.tsx
@@ -33,37 +33,29 @@ interface addItemData {
 export const addItem = async (data: addItemData) => {
     await API.post("/api/items/", data).
     then((response: any) => {
-        // toast.success(response.data.msg || "Item created successfully");
         return response.data
     }).
     catch((error: any) => {
-        // toast.error(error.response.data.message || error.response.data.msg || "Failed to create item");
         throw error.response
     });
 }
 
 export const updateItem = async (data: addItemData, id: string | undefined) => {
-    let is_Success = false
     await API.put(`/api/items/${id}/`, data).
     then((response: any) => {
-        toast.success(response.data.msg || "Item updated successfully");
-        is_Success = true
+        return response.data
     }).
     catch((error: any) => {
-        toast.error(error.response.data.message || error.response.data.msg || "Failed to update item");
+        throw error.response
     });
-    return is_Success
 }
 
 export const deleteItem = async (id: string | undefined) => {
     await API.delete(`/api/items/${id}/`).
     then((response: any) => {
-        // toast.success(response.data.msg || "Item deleted successfully");
-        // is_Success = true
         return response
     }).
     catch((error: any) => {
-        // toast.error(error.response.data.message || error.response.data.msg || "Failed to delete item");
         return error
     });
 }
