9f2173761de71f0d8b97292216a1200473f45c00
Hannah <hannahg.florano@gmail.com>
2025-04-07 00:04:12 +0800

    little changes

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from src import app as test_app, db
 @pytest.fixture()
 def app():
-    test_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory"
+    test_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:test.sqlite3"
     test_app.config["TESTING"] = True
 
     with test_app.app_context():
@@ -10,9 +10,6 @@ def app():
     
     yield test_app
 
-    with test_app.app_context():
-        db.drop_all()
-
 @pytest.fixture()
 def client(app):
-    return app.test_client()
+    return test_app.test_client()
