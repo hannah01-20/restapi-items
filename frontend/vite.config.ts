7cabe97c8b36e943615913190df9aced2f458776
Hannah <hannahg.florano@gmail.com>
2025-05-05 13:06:39 +0800

    frontend fixing routing base(2)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [
     TanStackRouterVite({
       target: "react",
