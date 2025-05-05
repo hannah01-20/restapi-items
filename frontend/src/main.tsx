bf9794548f5efe759a0d3b20cea0e3f0796edef1
Hannah <hannahg.florano@gmail.com>
2025-05-05 12:28:51 +0800

    frontend preparing for hosting changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,7 +12,7 @@ const router = createRouter({
   routeTree,
   defaultNotFoundComponent: NotFound,
   defaultPendingComponent: Spinner,
-  basepath: '/restapi-items/',
+  // basepath: '/restapi-items/',
  })
 
 // Register the router instance for type safety
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/restapi-items/",
   plugins: [
     TanStackRouterVite({
       target: "react",
